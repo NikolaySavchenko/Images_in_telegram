a11914411769b82b3ce5160135be5746a9813704
Nikolay Savchenko <7548568@gmail.com>
2022-09-18 21:57:12 +0300

    publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ import os
 
 if __name__ == '__main__':
     load_dotenv()
-    space_bot = telegram.Bot(token=os.getenv('BOT_TOKEN'))
-    # updates = space_bot.get_updates()
-    # space_bot.send_message(text="Hi Peopple! I'm new admin!", chat_id='@space_view')
+    space_view_bot = telegram.Bot(token=os.getenv('BOT_TOKEN'))
+    # updates = space_view_bot.get_updates()
+    # space_view_bot.send_message(text="Hi Peopple! I'm new admin!", chat_id='@space_view')
+    space_view_bot.send_photo(chat_id='@space_view', photo=open('images/nasa6.jpg', 'rb'))
