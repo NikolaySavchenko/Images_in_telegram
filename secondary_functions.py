27342926aea0d24e60fe759ccc3800a02a24674d
Nikolay Savchenko <7548568@gmail.com>
2022-09-22 21:40:38 +0300

    Using None in function

--- secondary_functions.py
+++ secondary_functions.py
@@ -9,8 +9,8 @@ def file_extension(url):
     return urlsplit(split_url[1]).path
 
 
-def getting_page(url, file_name, playload={}, folder_name='images'):
-    Path('folder_name').mkdir(parents=True, exist_ok=True)
+def getting_page(url, file_name, playload=None, folder_name='images'):
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
     response = requests.get(url, params=playload)
     response.raise_for_status()
     with open(f'{folder_name}/{file_name}{file_extension(url)}', 'wb') as file:
