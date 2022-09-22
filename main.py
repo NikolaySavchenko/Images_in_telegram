b53115d8743dcf11957afa4dd3c68253e06c52cf
Nikolay Savchenko <7548568@gmail.com>
2022-09-22 21:47:08 +0300

    Added to main a chat change and tokens

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ from fetch_NASA_images import fetch_nasa_images, fetch_EPIC_images
 from telegram_bot import reporter_bot
 from dotenv import load_dotenv
 import argparse
+import os
 
 
 def main():
     load_dotenv()
-    fetch_spacex_launch()
-    fetch_nasa_images()
-    fetch_EPIC_images()
-    parser = argparse.ArgumentParser('Input delay time sec')
+    # fetch_spacex_launch()
+    nasa_token = os.environ['NASA_TOKEN']
+    # fetch_nasa_images(nasa_token)
+    # fetch_EPIC_images(nasa_token)
+    parser = argparse.ArgumentParser('Input delay time sec, chat ID')
     parser.add_argument('delay', nargs='?', default='14400')
-    delay_time = parser.parse_args().delay
-    reporter_bot(int(delay_time))
+    parser.add_argument('chat_id', nargs='?', default='@space_view')
+    delay_time = int(parser.parse_args().delay)
+    chat_id = parser.parse_args().chat_id
+    bot_token = os.environ['BOT_TOKEN']
+    reporter_bot(bot_token, delay_time, chat_id)
 
 
 if __name__ == '__main__':
