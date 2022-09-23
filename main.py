227afaefa50ab0769fcb3b331ec361361b08502f
Nikolay Savchenko <7548568@gmail.com>
2022-09-23 21:01:24 +0300

    Rename environment variable

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     parser.add_argument('chat_id', nargs='?', default='@space_view')
     delay_time = int(parser.parse_args().delay)
     chat_id = parser.parse_args().chat_id
-    bot_token = os.environ['BOT_TOKEN']
+    bot_token = os.environ['TG_BOT_TOKEN']
     reporter_bot(bot_token, delay_time, chat_id)
 
 
