51dcece1a07dc5fb43f4e2e70fda28fc8f79e843
Nikolay Savchenko <7548568@gmail.com>
2022-09-23 21:06:58 +0300

    Rename variables

--- fetch_NASA_images.py
+++ fetch_NASA_images.py
@@ -6,9 +6,9 @@ import argparse
 from datetime import datetime
 
 
-def fetch_nasa_images(nasa_token, count_img=10):
+def fetch_nasa_images(nasa_token, images_count=10):
     nasa_dayfoto_url = 'https://api.nasa.gov/planetary/apod'
-    playload = {'api_key': nasa_token, 'count': count_img}
+    playload = {'api_key': nasa_token, 'count': images_count}
     response = requests.get(nasa_dayfoto_url, params=playload)
     response.raise_for_status()
     links = response.json()
@@ -22,11 +22,11 @@ def fetch_EPIC_images(nasa_token):
     response = requests.get(EPIC_url, params=playload)
     response.raise_for_status()
     links = response.json()
-    for pictere_number, link in enumerate(links):
+    for picture_number, link in enumerate(links):
         date_link = datetime.strptime(link['date'],
                                       "%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d")
         earth_url = f'https://api.nasa.gov/EPIC/archive/natural/{date_link}/png/{link["image"]}.png'
-        getting_page(earth_url, f'EPIC{pictere_number}', playload)
+        getting_page(earth_url, f'EPIC{picture_number}', playload)
 
 
 if __name__ == '__main__':
