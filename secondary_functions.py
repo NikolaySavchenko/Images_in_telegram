b75beadcaa5e8079294aac3df890a0b22c7c82c4
Nikolay Savchenko <7548568@gmail.com>
2022-09-27 09:06:23 +0300

    Take out new secondary functions
    Walking for files
    Retrying bot action

--- secondary_functions.py
+++ secondary_functions.py
@@ -2,6 +2,8 @@ import requests
 import os
 from urllib.parse import urlsplit
 from pathlib import Path
+from time import sleep
+import telegram
 
 
 def file_extension(url):
@@ -9,9 +11,30 @@ def file_extension(url):
     return urlsplit(split_url[1]).path
 
 
-def getting_page(url, file_name, playload=None, folder_name='images'):
+def getting_page(url, file_name, payload=None, folder_name='images'):
     Path(folder_name).mkdir(parents=True, exist_ok=True)
-    response = requests.get(url, params=playload)
+    response = requests.get(url, params=payload)
     response.raise_for_status()
     with open(Path(f'{folder_name}/{file_name}{file_extension(url)}'), 'wb') as file:
         file.write(response.content)
+
+
+def walking_for_files(folder_adress):
+    files = os.walk(folder_adress)
+    for file_list in files:
+        return file_list[2]
+
+def retrying_bot_action(bot_name, chat_id, file, Error_sleep=10, delay_time_sec):
+    try:
+        bot_name.send_photo(chat_id=chat_id, photo=file)
+        sleep(delay_time_sec)
+    except telegram.error.NetworkError:
+        print(f'NetworkError, Resend after {Error_sleep} sec')
+        sleep(Error_sleep)
+        try:
+            bot_name.send_photo(chat_id=chat_id, photo=file)
+            sleep(delay_time_sec)
+        except telegram.error.NetworkError:
+            print(f'NetworkError, Resend after {Error_sleep*15} sec')
+            sleep(Error_sleep*15)
+            return
