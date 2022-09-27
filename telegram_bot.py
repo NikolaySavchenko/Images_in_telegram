18401be7ce5a7520bdb8c38d61dbef7dda11ae77
Nikolay Savchenko <7548568@gmail.com>
2022-09-27 09:09:18 +0300

    Corrected reporter_bot with new functions
    And make chat_id required argument

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,40 +1,29 @@
 import telegram
 from dotenv import load_dotenv
 from random import shuffle
-from time import sleep
 from pathlib import Path
 import argparse
 import os
+from secondary_functions import walking_for_files
+from secondary_functions import retrying_bot_action
 
 
-def reporter_bot(bot_token, delay_time_sec=14400, chat_id='@space_view'):
+def reporter_bot(bot_token, chat_id, delay_time_sec=14400):
     space_view_bot = telegram.Bot(token=bot_token)
-    images = os.walk('images')
     while True:
-        for images_list in images:
-            shuffle(images_list[2])
-            for image in images_list[2]:
-                with open(Path(f'images/{image}'), 'rb') as file:
-                    try:
-                        space_view_bot.send_photo(chat_id=chat_id, photo=file)
-                    except telegram.error.NetworkError:
-                        print('NetworkError, Resend after 10 seconds')
-                        sleep(10)
-                        try:
-                            space_view_bot.send_photo(chat_id=chat_id, photo=file)
-                        except telegram.error.NetworkError:
-                            print('NetworkError, Resend after 2 min')
-                            sleep(120)
-                            continue
-                sleep(delay_time_sec)
+        images_list = walking_for_files('images')
+        shuffle(images_list)
+        for image in images_list:
+            with open(Path(f'images/{image}'), 'rb') as file:
+                retrying_bot_action(space_view_bot, chat_id, file, delay_time_sec)
 
 
 if __name__ == '__main__':
     load_dotenv()
     bot_token = os.environ['TG_BOT_TOKEN']
-    parser = argparse.ArgumentParser('Input delay time sec, chat ID')
+    parser = argparse.ArgumentParser('Input chat ID, delay time sec')
+    parser.add_argument('chat_id')
     parser.add_argument('delay', nargs='?', default=14400)
-    parser.add_argument('chat_id', nargs='?', default='@space_view')
     delay_time = int(parser.parse_args().delay)
     chat_id = parser.parse_args().chat_id
-    reporter_bot(bot_token, delay_time, chat_id)
+    reporter_bot(bot_token, chat_id, delay_time)
